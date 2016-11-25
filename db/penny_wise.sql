583c70a8fbc44b1bb46de9b1435bd9a9316bcaf0
Leelz <adam.leel93@gmail.com>
2016-11-25 13:12:44 +0000

    created the penny wise database

---
+++ db/penny_wise.sql
@@ -0,0 +1,24 @@
+DROP TABLE transactions;
+DROP TABLE merchants;
+DROP TABLE categories;
+
+CREATE TABLE categories
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255) not null,
+);
+
+CREATE TABLE merchants
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255) not null,
+);
+
+CREATE TABLE transactions
+(
+  id SERIAL8 primary key,
+  merchant_id INT8 references merchants(id),
+  category_id INT8 references categories(id),
+  value INT8,
+  transaction_date date
+);
