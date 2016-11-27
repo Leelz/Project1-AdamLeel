f40c4bdcff45bbcd398d458c8378e767e3ab41c0
Leelz <adam.leel93@gmail.com>
2016-11-27 13:55:12 +0000

    edited comment out of transaction controller

--- controllers/transaction_controller.rb
+++ controllers/transaction_controller.rb
@@ -6,7 +6,6 @@ require_relative( '../models/transaction.rb' )
 
 #list all the transactions
 get '/transactions' do
-  # return "transactions"
   @transactions = Transaction.all()
   erb(:index)
 end
